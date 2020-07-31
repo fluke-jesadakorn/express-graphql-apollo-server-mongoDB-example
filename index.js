ffafe8e4e0d7e7f70c942efad78f838e9f7eb4df
fluke-jesadakorn <54740058+fluke-jesadakorn@users.noreply.github.com>
2020-08-01 03:16:47 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mongooose = require('mongoose')
+const { ApolloServer } = require('apollo-server-express')
+const typeDefs = require("./graphql/typeDefs")
+const resolvers = require('./graphql/resolvers')
+require('dotenv').config
+
+const PORT = process.env.PORT || 7000
+
+const createServer = async () => {
+    try {
+        await mongooose.connect(`mongodb://localhost:27017/test`, { useNewUrlParser: true, useUnifiedTopology: true })
+            .then((_e) => console.log(`MongoDB connected`))
+
+        const server = new ApolloServer({
+            typeDefs,
+            resolvers
+        })
+
+        server.applyMiddleware({ app })
+
+        app.listen({ port: PORT }, () => console.log(`
+            Server running on http://localhost : ${PORT} ${server.graphqlPath}
+        `))
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+createServer();
